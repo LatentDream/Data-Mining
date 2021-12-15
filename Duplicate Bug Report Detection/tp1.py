937a7c8ecf263ebcf324b0fdd0480f87d0756730
Guillaume <g.thibault@polymtl.ca>
2021-12-14 23:38:30 -0500

    Added Projet

---
+++ Duplicate Bug Report Detection/tp1.py
@@ -0,0 +1,13 @@
+# python
+import numpy as np
+import sklearn as sk
+import scipy
+import nltk
+import tqdm
+import nltk
+
+nltk.download("punkt")
+
+from IPython import display
+
+display.Image("https://irving-muller.github.io/images/bug-report.png")
